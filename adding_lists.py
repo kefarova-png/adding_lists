73f24a3066fa4a2a571f81a46f6c72c6174899c7
kefarova-png <kefarova@gmail.com>
2026-03-31 15:55:53 +0400

    Добавлен ввод списков пользователем

--- adding_lists.py
+++ adding_lists.py
@@ -1,31 +1,28 @@
-def input_lenght_of_lists():
-    text_of_error = '''Ошибка: невозможно создать списки заданной длины.
-В следующий раз введите, пожалуйста, целое положительное число.'''
+def input_list(list_number):
     while True:
         try:
-            lenght = int(input('Введите длину генерируемых списков (целое положительное число): '))
-            if lenght > 0:
-                return lenght
-            print(text_of_error)
+            input_str = input(f"Введите числа {list_number}-го списка через пробел: ")
+            list_str = input_str.split()
+            list_i = [float(num) for num in list_str]
+            if len(list_i) > 0:
+                print(f"Ваш {list_number}-й список чисел:", list_i)
+                return list_i
+            print('Введённые данные не содержат чисел.')
         except ValueError:
-            print(text_of_error)
+            print('''Ошибка: невозможно создать список по введенным данным.
+В следующий раз вводите, пожалуйста, только числа и пробелы.''')
 
 
-def generator_of_lists(lenght):
-    list_1 = [(x+1)*10 for x in range(0, lenght)]
-    list_2 = [(x+1)*(-1) for x in range(0, lenght)]
-    return list_1, list_2
-
-
-def adding_list_elements(list_1, list_2, lenght):
+def adding_list_elements(list_1, list_2):
+    min_lenght = min(len(list_1), len(list_2))
     sum_list = []
-    for n in range(0, lenght):
+    for n in range(0, min_lenght):
         sum_element = list_1[n] + list_2[n]
         sum_list.append(sum_element)
     return sum_list
 
 
-lenght = input_lenght_of_lists()
-list_1, list_2 = generator_of_lists(lenght)
-sum_list = adding_list_elements(list_1, list_2, lenght)
-print(sum_list)
+list_1 = input_list(1)
+list_2 = input_list(2)
+sum_list = adding_list_elements(list_1, list_2)
+print("Ваши суммы чисел", sum_list)
